b5fd17a44811295a1c6e31c26e8351818ed6508a
Namlongcomputer <hoandm91@gmail.com>
2019-03-26 18:35:28 +0700

    version4

--- Shinhtron.js
+++ Shinhtron.js
@@ -2,4 +2,7 @@ let r = parseInt(prompt("Nhap ban kinh hinh tron: ", ''));
 document.write("r = " + r);
 document.write('<br/>');
 let S = 3.14*r*r;
+let CV = 2*3.14*r;
 document.write("Dien tich hinh tron S = " + S);
+document.write('<br/>');
+document.write("Chu vi hinh tron CV = " + CV);
